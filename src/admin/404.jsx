f29996a92ac81b35bf224f304b4dce1eb5787696
mark <mark.marteus5@gmail.com>
2024-08-15 14:24:05 +0200

    404 page update

--- src/admin/404.jsx
+++ src/admin/404.jsx
@@ -1,12 +1,48 @@
-import { Box, Center } from '@chakra-ui/react'
+import { Box, Text, Button, VStack, Image } from '@chakra-ui/react';
 import React from 'react'
 
 export default function NotFound() {
     return (
-        <Box bg={'#000018'} minH={'100vh'}>
-            <Center>
-                <Box mt={'30vh'} fontSize={'4xl'} as='kbd' color={'white'}>404 Page not Found</Box>
-            </Center>
-        </Box>
+        <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+        backgroundColor="black"
+        color="white"
+        position="relative"
+        overflow="hidden"
+      >
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          backgroundImage="url('/admin/img/404.png')"
+          backgroundSize="cover"
+          opacity="0.5"
+        />
+        <VStack zIndex="1" spacing={4}>
+          <Text fontSize="10rem" fontWeight="bold" color="red.500">
+            404
+          </Text>
+          <Text fontSize="2xl" textAlign="center">
+            OOPS PAGE NOT FOUND
+          </Text>
+          <Button colorScheme="red" size="lg" onClick={() => window.location.href = '/'}>
+            HOME 
+          </Button>
+        </VStack>
+        <Image
+          src="/admin/img/404.png"
+          alt="Decorative Image"
+          position="absolute"
+          bottom="0"
+          right="0"
+          width="200px"
+        />
+      </Box>
     )
 }
