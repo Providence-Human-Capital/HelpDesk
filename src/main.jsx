a3c8150a4f8554cd13f6a8cc46ec9d84b3282819
mafaro21 <mafaro2105@gmail.com>
2024-08-20 14:57:23 +0200

    [fix] re-did routing to allow deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,64 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import {ReactQueryDevTools}
-import Login from './admin/login.jsx'
-import Dashboard from './admin/dashboard.jsx'
-import Manage from './admin/manage.jsx'
-import General from './admin/generalRequests.jsx'
-import NotFound from './admin/404.jsx'
+import Login from './admin/login.jsx';
+import Dashboard from './admin/dashboard.jsx';
+import Manage from './admin/manage.jsx';
+import General from './admin/generalRequests.jsx';
+import NotFound from './admin/404.jsx';
 
-const queryClient = new QueryClient({})
+const queryClient = new QueryClient({});
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/admin",
-    element: <Login />,
-  },
-  {
-    path: "/admin/dash",
-    element: <Dashboard />,
-  },
-  {
-    path: "/admin/management",
-    element: <Manage />,
-  },
-  {
-    path: "/admin/:request",
-    element: <General />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/admin",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/admin/dash",
+//     element: <Dashboard />,
+//   },
+//   {
+//     path: "/admin/management",
+//     element: <Manage />,
+//   },
+//   {
+//     path: "/admin/:request",
+//     element: <General />,
+//   },
+//   {
+//     path: '*',
+//     element: <NotFound />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   <ChakraProvider>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      {/* < */}
+      <BrowserRouter basename='/helpdesk'>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin" element={<Login />} />
+          <Route path="/admin/dash" element={<Dashboard />} />
+          <Route path="/admin/management" element={<Manage />} />
+          <Route path="/admin/:request" element={<General />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </ChakraProvider>
-  // </React.StrictMode>,
-)
+);
